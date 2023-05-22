6e844f03695de05a1a00d2857fc2a0a435f6a9a6
laylo <lbosimova14@gmail.com>
2023-05-22 16:54:32 -0400

    added css, webfonts, flags, js files

--- src/main/java/com/cydeo/entity/User.java
+++ src/main/java/com/cydeo/entity/User.java
@@ -4,28 +4,27 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Entity
 @Table(name = "users")
-public class User extends BaseEntity{
+public class User extends BaseEntity {
 
     @Column(unique = true)
-   private String username;
-    private String password ;
-    private String firstname ;
+    private String username;
+    private String password;
+    private String firstname;
     private String lastname;
     private String phone;
     private boolean enabled;
     @ManyToOne
+    @JoinColumn(name = "role_id")
     private Role role;
     @ManyToOne
+    @JoinColumn(name = "company_id")
     private Company company;
 
 }
